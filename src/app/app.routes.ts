f352f1667cff68bd67c45577c80e413531a4ce45
christopher espejo <criskevin20@gmail.com>
2021-03-07 17:51:53 -0500

    lacsm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+
 
 
 const APP_ROUTES: Routes = [
     
         {path: 'home', component:HomeComponent},
+        {path: 'banner', component: BannerComponent},
         {path: 'header', component:HeaderComponent},
         {path: 'footer', component:FooterComponent},
         {path: '**', pathMatch: 'full', redirectTo:'home'}
